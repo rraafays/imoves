4b58551afa9ec47d5fe31dcd67b941cae7cb5b8e
raf <rraf@tuta.io>
2023-01-24 18:24:51 +0000

    update: added firebase and hard requirement for a credentials file, started commenting

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; // make expo aware of device statusbar
+import { StyleSheet, Text, View } from 'react-native'; // stylsheet for theming, text for text, view for displaying things
+
+import firebase from 'firebase/app'; // ability to establish connection to firebase
+
+import credentials from './credentials' // email rraf@tuta.io for credentials file
+if (firebase.apps.length == 0) { credentials } // if firebase is not running, run it
 
 export default function App() {
   return (
