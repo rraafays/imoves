5c5e063cf421c0eef26df62a0d894740de7cc533
raf <rraf@tuta.io>
2023-01-31 15:02:37 +0000

    update: deployed user storing to firebase

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,13 @@
 const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const root = require('firebase-admin')
+root.initializeApp()
+
+const database = root.firestore()
+
+exports.new_user = functions.auth.user().onCreate((user) => {
+  return database
+    .collection('user')
+    .doc(user.uuid)
+    .create(JSON.parse(JSON.stringify(user)))
+})
