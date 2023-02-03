b97ac0cb0ffce56c6f7ca31999e63393f3ce2dfa
raf <rraf@tuta.io>
2023-02-03 15:22:31 +0000

    update: temporary implementation of tabbed bottom bar with icons and page routing

--- src/navigation/home/index.js
+++ src/navigation/home/index.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+const Tab = createMaterialBottomTabNavigator()
+
+const TEST_SCREEN = () => {
+  return (<View><Text>i am a placeholder</Text></View>)
+}
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>i am home</Text>
-    </View>
+    <Tab.Navigator
+      barStyle={{ backgroundColor: '#000000' }}
+      initialRouteName='feed'
+      activeColor='#E0E0E0'
+      inactiveColor='#E0E0E0'
+      shifting={true}
+    >
+      <Tab.Screen
+        name='Feed'
+        component={TEST_SCREEN}
+        options={{ tabBarIcon: () => <MaterialCommunityIcons name='home' size={24} color={'#702F8A'} /> }}
+      />
+      <Tab.Screen
+        name='Add'
+        component={TEST_SCREEN}
+        options={{ tabBarIcon: () => <MaterialCommunityIcons name='plus-circle' circle size={24} color={'#702F8A'} /> }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={TEST_SCREEN}
+        options={{ tabBarIcon: () => <MaterialCommunityIcons name='baby-face' size={24} color={'#702F8A'} /> }}
+      />
+    </Tab.Navigator>
   )
 }
